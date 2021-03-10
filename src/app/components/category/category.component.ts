08077f17fc392c8334b3e044f20a42eaacafb960
şükrü akkoyun <Sukruakkoyun87@hotmail.com>
2021-03-10 21:57:30 +0300

    10 Mart Ders Arası

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
+ 
+
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    productId:number;
+    categoryId:number;
+    productName:string;
+    unitsInStok:number;
+    unitPrice:number;
+}
